68e6884d580fdd99c4ae7fb8c17f9fe55ecd89ae
luochao <luochao@joyy.sg>
2021-07-25 14:26:54 +0800

    fix: 移除没有必要的注释

--- packages/addEvent/src/index.js
+++ packages/addEvent/src/index.js
@@ -8,12 +8,14 @@
  */
 export default function addEvent (obj, type, fn, useCapture) {
   // @credit http://stackoverflow.com/questions/2657182/correct-usage-of-addeventlistener-attachevent
-  // @credit http://dustindiaz.com/rock-solid-addevent
   if (obj.addEventListener) {
     obj.addEventListener(type, fn, !!useCapture)
-  } else if (obj.attachEvent) { // IE6-7
+  } else if (obj.attachEvent) {
+    // IE6-7
     obj['e' + type + fn] = fn
-    obj[type + fn] = function () { obj['e' + type + fn](window.event) }
+    obj[type + fn] = function () {
+      obj['e' + type + fn](window.event)
+    }
     obj.attachEvent('on' + type, obj[type + fn])
   }
 }
